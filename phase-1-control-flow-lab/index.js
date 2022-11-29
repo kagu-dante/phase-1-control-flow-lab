e253b1c84702ae5ad80fa19c3b0252afa7334799
Daniel Njuguna <117805781+kagu-dante@users.noreply.github.com>
2022-11-29 13:58:54 +0300

    Add files via upload

---
+++ phase-1-control-flow-lab/index.js
@@ -0,0 +1,35 @@
+function scuberGreetingForFeet(){
+  // Write your code here!
+  if (distance <= 400)
+  return "This one is on me!";
+  else if (distance >2000 && distance <2500){
+    return "I will gladly take your thirty bucks.";
+    }
+  else if(distance >2500){
+    return ("No can do.");
+  }
+}
+
+function ternaryCheckCity(){
+  // Write your code here!
+  if (City ==="NYC"){
+    return "Ok, sounds good.";
+    }
+    else {
+      return "No go.";
+    }
+}
+
+function switchOnCharmFromTip(){
+  // Write your code here!
+  if (Tip === "generous"){
+    return "Thank you so much.";
+
+    }
+    else if (Tip === "not as generous"){
+      return "Thank you.";
+    }
+    else {
+     return "Bye.";
+    }
+}
